1843b57eb504a278eb01c93cd305eec108455a70
Uday Jadhav <uday.jadhav@myob.com>
2020-11-19 17:59:31 +1100

    Fix test error

--- OrderFulfilment/OrderFulfilment.Tests/UserDetailsRepositoryTests.cs
+++ OrderFulfilment/OrderFulfilment.Tests/UserDetailsRepositoryTests.cs
@@ -1,4 +1,6 @@
 using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
 using WX.OrderFulfilment.Repository;
 using Xunit;
 
@@ -6,13 +8,20 @@ namespace WX.OrderFulfilment.Tests
 {
     public class UserDetailsRepositoryTests
     {
+        private readonly Mock<IConfiguration> _configuration;
+
+        public UserDetailsRepositoryTests()
+        {
+            _configuration = new Mock<IConfiguration>();
+        }
+
         [Fact]
         public void Should_Return_Basic_Details()
         {
             // Arrange
             const string expectedName = "Uday";
             const string expectedToken = "1234-455662-22233333-3333";
-            var repository = new OrderDetailsRepository();
+            var repository = new OrderDetailsRepository(_configuration.Object);
 
             // Act
             var userDetails = repository.GetUserDetails();
