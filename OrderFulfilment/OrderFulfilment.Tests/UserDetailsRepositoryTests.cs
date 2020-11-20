5593317f57e089472773e1ab623fab289330933c
Uday Jadhav <uday.jadhav@myob.com>
2020-11-20 17:29:14 +1100

    Fix test

--- OrderFulfilment/OrderFulfilment.Tests/UserDetailsRepositoryTests.cs
+++ OrderFulfilment/OrderFulfilment.Tests/UserDetailsRepositoryTests.cs
@@ -13,14 +13,15 @@ namespace WX.OrderFulfilment.Tests
         public UserDetailsRepositoryTests()
         {
             _configuration = new Mock<IConfiguration>();
+            ConfigureTestSettings();
         }
 
         [Fact]
         public void Should_Return_Basic_Details()
         {
             // Arrange
-            const string expectedName = "Uday";
-            const string expectedToken = "1234-455662-22233333-3333";
+            const string expectedName = "Uday Jadhav";
+            const string expectedToken = "276b32a9-8e35-4981-87b4-85e0a30f3319";
             var repository = new OrderDetailsRepository(_configuration.Object);
 
             // Act
@@ -30,5 +31,20 @@ namespace WX.OrderFulfilment.Tests
             userDetails.Name.Should().Be(expectedName);
             userDetails.Token.Should().Be(expectedToken);
         }
-    }
+
+        private void ConfigureTestSettings()
+        {
+            var tokenSection = new Mock<IConfigurationSection>();
+            tokenSection.Setup(a => a.Value).Returns("276b32a9-8e35-4981-87b4-85e0a30f3319");
+            _configuration
+                .Setup(config => config.GetSection("UserDetails:Token"))
+                .Returns(tokenSection.Object);
+
+            var nameSection = new Mock<IConfigurationSection>();
+            nameSection.Setup(a => a.Value).Returns("Uday Jadhav");
+            _configuration
+                .Setup(config => config.GetSection("UserDetails:Name"))
+                .Returns(nameSection.Object);
+        }
+        }
 }
